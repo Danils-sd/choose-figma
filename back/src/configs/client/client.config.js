819a21311d89937fe649466e78da48e47fc2235b
Danils-sd <hlebushek.sd@gmail.com>
2024-03-24 16:18:41 +0300

    client+admin configs

---
+++ back/src/configs/client/client.config.js
@@ -0,0 +1,34 @@
+const { initializeApp } = require("firebase/app");
+const { getAuth, signInWithEmailAndPassword, createUserWithEmailAndPassword} = require("firebase/auth");
+
+const config = require("./choose-figma-client-key.json");
+
+const app = initializeApp(config);
+
+const auth = getAuth(app);
+
+async function singIn(email, password){
+    try{
+        const user = await signInWithEmailAndPassword(auth, email, password);
+        return user._tokenResponse.localId;
+    } catch(error) {
+        console.log(error);
+        return -1;
+    }
+}
+
+async function create(email, password){
+    try {
+        const user = createUserWithEmailAndPassword(auth, email, password);
+        return user._tokenResponse.localId;
+
+    } catch (error) {
+        console.log(error);
+        return -1;
+    }
+}
+
+module.exports = {
+    singIn,
+    create
+}
